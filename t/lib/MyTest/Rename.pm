e2a63d5e2abb60ad069b97030dad2cb37319abbc
Alex Balhatchet <kaoru@slackwise.net>
2014-04-18 19:04:47 +0100

    Test for the rename feature

---
+++ t/lib/MyTest/Rename.pm
@@ -0,0 +1,18 @@
+package MyTest::Rename;
+
+use strict;
+use warnings;
+
+use Test::Kit2;
+
+Test::Kit2->include({
+    'Test::More' => {
+        rename => {
+            ok => 'is_true',
+            is => 'equal',
+            pass => 'ok', # evil!
+        },
+    },
+});
+
+1;
